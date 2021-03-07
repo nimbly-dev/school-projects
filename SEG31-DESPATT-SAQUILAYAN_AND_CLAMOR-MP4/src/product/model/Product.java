bc069fbe0f1c8877b93a6f7d8bb7cd29c505a2b0
nimbly-dev <61898119+nimbly-dev@users.noreply.github.com>
2021-03-07 22:42:39 +0800

    Delete SEG31-DESPATT-SAQUILAYAN_AND_CLAMOR-MP4 directory

--- SEG31-DESPATT-SAQUILAYAN_AND_CLAMOR-MP4/src/product/model/Product.java
+++
@@ -1,9 +0,0 @@
-package product.model;
-
-import productType.model.ProductType;
-
-public abstract class Product implements ProductItem{
-	
-	//PLACEHOLDER PARENT PRODUCT CLASS
-	public abstract ProductType productType();
-}
--- SEG31-DESPATT-SAQUILAYAN_AND_CLAMOR-MP4/src/productType/model/ProductTypeItem.java
+++
@@ -1,9 +0,0 @@
-package productType.model;
-
-import packaging.model.Wrapper;
-
-public interface ProductTypeItem {
-	public int productTypeId();
-	public String productTypeName();
-	public Wrapper wrapper();
-}
